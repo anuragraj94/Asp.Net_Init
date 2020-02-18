66a440a8d91bfe4f2441398bbcdb32c0dceb22b2
anuragraj94 <anuragraj94@gmail.com>
2020-02-18 18:11:28 +0530

    Added : Project with database and notes

---
+++ WebApplication1/Demo.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        public static string Name;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }      
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Name = TextBox1.Text;
+            Session["Name"]= TextBox1.Text;
+            Response.Write(TextBox1.Text + "</br>");
+            Response.Write(ListBox1.SelectedItem.Text + "</br>");
+
+            Label1.Visible = false;
+            TextBox1.Visible = false;
+            ListBox1.Visible = false;
+            CheckBox1.Visible = false;
+            CheckBox2.Visible = false;
+            RadioButton1.Visible = false;
+            RadioButton2.Visible = false;
+            Button1.Visible = false;
+        }
+    }
+}
