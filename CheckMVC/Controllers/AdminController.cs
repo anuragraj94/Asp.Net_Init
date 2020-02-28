071a3f69e61c5d9f73de0e6f1190d6f2ed427236
anuragraj94 <anuragraj94@gmail.com>
2020-02-28 13:21:00 +0530

    Mvc template added

---
+++ CheckMVC/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AddWareHouse()
+        {
+            return View();
+        }
+        public ActionResult OrderCargo()
+        {
+            return View();
+        }
+        public ActionResult ShiftOrder()
+        {
+            return View();
+        }
+        public ActionResult ViewOrder()
+        {
+            return View();
+        }
+        public ActionResult TraceCargo()
+        {
+            return View();
+        }
+        public ActionResult Reorder()
+        {
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ CheckMVC/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Acceptance()
+        {
+            return View();
+        }
+        public ActionResult Dispatch()
+        {
+            return View();
+        }
+        public ActionResult DamageGoods()
+        {
+            return View();
+        }
+        public ActionResult GatePass()
+        {
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
