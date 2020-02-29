266e2ac2dd445620ef32e7c31548ce7000609cf8
anuragraj94 <anuragraj94@gmail.com>
2020-02-29 17:20:53 +0530

    Login css modified

--- CheckMVC/Global.asax.cs
+++ CheckMVC/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace CheckMVC
@@ -13,6 +14,7 @@ namespace CheckMVC
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            App_Start.BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
